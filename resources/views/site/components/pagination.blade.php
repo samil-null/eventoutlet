33daf42407f6a8d9a168e4615b4a90667931b34a
samil-null <denis.budancev@gmail.com>
2020-07-21 22:05:31 +0300

    fix pagination

--- resources/views/site/components/pagination.blade.php
+++ resources/views/site/components/pagination.blade.php
@@ -6,7 +6,7 @@
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                        <div class="catalog-pagination__item">...</div>
+                        <div class="catalog-pagination__item catalog-pagination__item_more">...</div>
                 @endif
 
                 {{-- Array Of Links --}}
@@ -24,3 +24,37 @@
         </div>
     </div>
 @endif
+
+@push('scripts')
+    <style>
+        @media (max-width:768px) {
+            .catalog-pagination__item {
+                display: none;
+            }
+
+            .catalog-pagination__item.show {
+                display: flex;
+            }
+
+            .catalog-pagination__item:first-child,
+            .catalog-pagination__item:last-child,
+            .catalog-pagination__item.active {
+                display: flex;
+            }
+        }
+    </style>
+    <script>
+        if (window.innerWidth < 768) {            
+            let paginationElement = document.querySelector('.catalog-pagination__item.active');
+            
+            if (paginationElement.nextElementSibling) {
+                paginationElement.nextElementSibling.classList.add('show');
+            }
+
+            if (paginationElement.previousElementSibling) {
+                paginationElement.previousElementSibling.classList.add('show');
+            }
+            
+        }
+    </script>
+@endpush
